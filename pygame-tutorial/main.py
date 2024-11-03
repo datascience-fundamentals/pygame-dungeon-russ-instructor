30610d4e73e60d4443ba9ed44a01942948290dc5
rolandoandre123 <rbardalespriale@gmail.com>
2024-11-03 18:21:26 -0500

    add : appying movement funcionality for character

--- pygame-tutorial/main.py
+++ pygame-tutorial/main.py
@@ -9,6 +9,9 @@ screen = pygame.display.set_mode((cons.SCREEN_WIDTH, cons.SCREEN_HEIGHT))
 # changing the game title
 pygame.display.set_caption("Dungeon Crawler")
 
+# create clock for maintaining frame rate
+clock = pygame.time.Clock()
+
 # define player movement variables
 moving_left = False
 moving_right = False
@@ -21,20 +24,25 @@ player = Character(100, 100)
 # main game loop
 run = True
 while run == True:
+    # control frame rate
+    clock.tick(cons.FPS)
+    screen.fill(cons.BG)
+
     # calculate player movement
     dx = 0
     dy = 0
 
     if moving_right == True:
-        dx = 5
+        dx = cons.SPEED
     if moving_left == True:
-        dx = -5
+        dx = -cons.SPEED
     if moving_up == True:
-        dy = -5
+        dy = -cons.SPEED
     if moving_down == True:
-        dy = 5
+        dy = cons.SPEED
 
-    print(f"{str(dx)} , {str(dy)}")
+    # move player
+    player.move(dx, dy)
 
     # draw player on screen
     player.draw(screen)
@@ -53,6 +61,16 @@ while run == True:
                 moving_up = True
             if event.key == pygame.K_s:
                 moving_down = True
+        # keyboard button released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+            if event.key == pygame.K_w:
+                moving_up = False
+            if event.key == pygame.K_s:
+                moving_down = False
 
     pygame.display.update()
 
